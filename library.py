21a9cc064d2e16598ec528ce1804e0cc63c1bf81
Michael Wiltshire <82546294+michael-wiltshire@users.noreply.github.com>
2022-02-01 15:14:56 -0800

    Update library.py

--- library.py
+++ library.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
-sklearn.impute import KNNImputer
+from sklearn.impute import KNNImputer
 
 class MappingTransformer(BaseEstimator, TransformerMixin):
 
