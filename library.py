16c695d32967a942983dff67921a0566a8426732
Michael Wiltshire <82546294+michael-wiltshire@users.noreply.github.com>
2022-02-21 20:34:34 -0800

    Update library.py

--- library.py
+++ library.py
@@ -275,10 +275,10 @@ def halving_search(model, grid, x_train, y_train, factor=3, scoring='roc_auc'):
   #your code below
   halving_cv = HalvingGridSearchCV(
     model, grid,  #our model and the parameter combos we want to try
-    scoring=scoring,  #could alternatively choose f1, accuracy or others
+    scoring='roc_auc',  #could alternatively choose f1, accuracy or others
     n_jobs=-1,
     min_resources="exhaust",
-    factor=factor,  #a typical place to start so triple samples and take top 3rd of combos on each iteration
+    factor=3,  #a typical place to start so triple samples and take top 3rd of combos on each iteration
     cv=5, random_state=1234,
     refit=True  #remembers the best combo and gives us back that model already trained and ready for testing
   )
