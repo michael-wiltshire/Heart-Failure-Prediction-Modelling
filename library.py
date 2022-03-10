64990da4b25de7b3f2bc8ded6b7768bc3153677e
Michael Wiltshire <82546294+michael-wiltshire@users.noreply.github.com>
2022-03-10 09:55:40 -0800

    imports update

--- library.py
+++ library.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
+from sklearn.linear_model import LogisticRegressionCV
 
 class MappingTransformer(BaseEstimator, TransformerMixin):
 
